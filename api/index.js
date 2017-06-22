99fcf09f7eeb1f6930e2dbf54dbf9de36ad19c94
Matthew Jiang <matthewljiang@gmail.com>
2017-06-22 00:24:09 -0400

    Set up api structure

---
+++ api/index.js
@@ -0,0 +1,8 @@
+var router = require('express').Router();
+
+router.get('/test', function (req,res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({a: 1}));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path'),
       indexPath = path.join(__dirname,'src','index.html');
       distPath = path.join(__dirname,'dist');
 
+const api = require('./api');
+
 app.use(express.static(distPath));
+
+app.use('/api', api);
+
 app.get('/', function (req, res) {
   res.sendFile(indexPath);
 });
